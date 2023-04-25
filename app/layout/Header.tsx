26e0404ae49d225caa5ee3c1b942231ec1d251e9
HoracioGutierrez <horacio.estevez@gmail.com>
2023-04-25 13:21:40 -0300

    Add planetscale .env url and prisma

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -13,18 +13,27 @@ export default function Header() {
   useEffect(() => {
     if (!user) {
       router.push('/sign-in')
-    }
+    } 
+    
   }, [user])
+
+  const handleApi = async () => {
+    const response = await fetch('/api/test')
+    const data = await response.json()
+    console.log(data)
+  }
 
   return (
     <header className="flex justify-between p-2">
       <h1>Title</h1>
-      <nav>
+      <nav className="flex gap-2">
+        <Link href="/demos"> demos </Link>
+        <button onClick={handleApi}>test api</button>
         <SignedIn>
           <UserButton />
         </SignedIn>
         <SignedOut>
-          <Link href="/sign-in"> Sign In </Link>
+          <Link href="/sign-in"> sign in </Link>
         </SignedOut>
       </nav>
     </header>
